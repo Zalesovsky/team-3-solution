edb21e77bd919a2371cfcdbfc279c895e8d9fcb2
Kuleb9lka <101733691+Kuleb9lka@users.noreply.github.com>
2024-02-03 14:20:59 +0300

    Revert "Create keyboards"

--- src/main/java/syberry/hackathon/bot/constant/Constant.java
+++ src/main/java/syberry/hackathon/bot/constant/Constant.java
@@ -2,25 +2,9 @@ package syberry.hackathon.bot.constant;
 
 public final class Constant {
 
-    private Constant(){
-    }
+    private Constant(){}
 
-    public static final String CURRENT_DAY_COURSE = "Курс на сегодняшний день";
-    public static final String SELECTED_DAY_COURSE = "Курс на выбранный день";
-    public static final String COLLECT_DATA = "Собрать статистику";
-    public static final String CHOOSE_DIFFERENT_BANK = "Выбрать другой банк";
-    public static final String CHOOSE_DIFFERENT_CURRENCY = "Выбрать другую валюту";
-    public static final String NATIONAL_BANK = "Национальный Банк";
-    public static final String ALPHA_BANK = "Альфа Банк";
-    public static final String BELARUS_BANK = "Беларусбанк";
+    public final static String BOT_TOKEN = "6534600404:AAEkHITYaI1tZ5_0Z6QCUgASd-wJOSpiEjc";
 
-    public static final String USD = "USD";
-    public static final String EUR = "EUR";
-    public static final String GBP = "GBP";
-    public static final String JPY = "JPY";
-
-
-
-
-
+    public final static String BOT_NAME = "Syberry_Bot_Java_Bot";
 }
