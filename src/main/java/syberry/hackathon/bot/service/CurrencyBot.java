e56b81ab98edf0fb687e22bd9685a41c5bbb068c
Zalesovsky <vladislav.zalesovski@gmail.com>
2024-02-03 14:26:18 +0300

    Reapply "Create keyboards"
    This reverts commit edb21e77bd919a2371cfcdbfc279c895e8d9fcb2.

---
+++ src/main/java/syberry/hackathon/bot/service/CurrencyBot.java
@@ -0,0 +1,121 @@
+package syberry.hackathon.bot.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import syberry.hackathon.bot.config.BotConfig;
+import syberry.hackathon.bot.constant.Constant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+
+public class CurrencyBot extends TelegramLongPollingBot {
+
+    @Autowired
+    private BotConfig config;
+
+    private String currentBank;
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+
+    @Override
+    public String getBotToken(){
+        return config.getBotToken();
+    }
+
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        Message message = update.getMessage();
+
+        if (message.hasText()){
+
+            if (message.getText().equals("/start")){
+
+                initGreetKeyboard(message);
+            }
+
+            initCurrencyKeyboard(message);
+
+
+
+
+        }
+
+    }
+
+
+    private void initGreetKeyboard(Message message){
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setResizeKeyboard(true);
+
+        List<KeyboardRow> rows = new ArrayList<>();
+
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add(new KeyboardButton(Constant.NATIONAL_BANK));
+
+        KeyboardRow row2 = new KeyboardRow();
+        row2.add(new KeyboardButton(Constant.ALPHA_BANK));
+        row2.add(new KeyboardButton(Constant.BELARUS_BANK));
+
+        rows.add(row1);
+        rows.add(row2);
+
+        replyKeyboardMarkup.setKeyboard(rows);
+
+        try {
+            execute(SendMessage.builder()
+                    .text("Привет! Чтобы воспользоваться фукциями бота сперва выбери банк из меню снизу.")
+                    .chatId(message.getChatId())
+                    .replyMarkup(replyKeyboardMarkup)
+                    .build());
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void initCurrencyKeyboard(Message message){
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setResizeKeyboard(true);
+
+        List<KeyboardRow> rows = new ArrayList<>();
+
+        KeyboardRow row1 = new KeyboardRow();
+        row1.add(new KeyboardButton(Constant.USD));
+        row1.add(new KeyboardButton(Constant.EUR));
+        row1.add(new KeyboardButton(Constant.GBP));
+        row1.add(new KeyboardButton(Constant.JPY));
+
+        rows.add(row1);
+
+        replyKeyboardMarkup.setKeyboard(rows);
+
+        try {
+            execute(SendMessage.builder()
+                    .text("Ты выбрал " + currentBank + ". Теперь выбери нужную тебе валюту из списка ниже.")
+                    .chatId(message.getChatId())
+                    .replyMarkup(replyKeyboardMarkup)
+                    .build());
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
