1b2495019e33ec13a3c76324812a98daf6d5282c
Kuleb9lka <bogdan.lopatenko@gmail.com>
2024-02-03 15:45:06 +0300

    changed constants and bot

---
+++ src/main/java/syberry/hackathon/bank/constants/BankConstants.java
@@ -0,0 +1,10 @@
+package syberry.hackathon.bank.constants;
+
+import java.util.Set;
+
+public final class BankConstants {
+
+    private BankConstants(){}
+    public static Set<String> banks = Set.of("Нацбанк Республики Беларусь", "Альфа-Банк", "Беларусбанк");
+
+}
--- src/main/java/syberry/hackathon/bank/service/BankService.java
+++ src/main/java/syberry/hackathon/bank/service/BankService.java
@@ -1,8 +1,5 @@
 package syberry.hackathon.bank.service;
 
-import syberry.hackathon.bank.constants.Constants;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public interface BankService {
