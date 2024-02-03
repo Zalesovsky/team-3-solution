672ba6f6413bde06130356210ff571f86e4e5724
Kuleb9lka <bogdan.lopatenko@gmail.com>
2024-02-03 14:07:45 +0300

    created greeet keyboard and currency keyboard

---
+++ src/main/java/syberry/hackathon/bot/constant/Constant.java
@@ -0,0 +1,26 @@
+package syberry.hackathon.bot.constant;
+
+public final class Constant {
+
+    private Constant(){
+    }
+
+    public static final String CURRENT_DAY_COURSE = "Курс на сегодняшний день";
+    public static final String SELECTED_DAY_COURSE = "Курс на выбранный день";
+    public static final String COLLECT_DATA = "Собрать статистику";
+    public static final String CHOOSE_DIFFERENT_BANK = "Выбрать другой банк";
+    public static final String CHOOSE_DIFFERENT_CURRENCY = "Выбрать другую валюту";
+    public static final String NATIONAL_BANK = "Национальный Банк";
+    public static final String ALPHA_BANK = "Альфа Банк";
+    public static final String BELARUS_BANK = "Беларусбанк";
+
+    public static final String USD = "USD";
+    public static final String EUR = "EUR";
+    public static final String GBP = "GBP";
+    public static final String JPY = "JPY";
+
+
+
+
+
+}
