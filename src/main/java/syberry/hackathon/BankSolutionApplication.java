b50fc40afe6b51e315fa13f820e4a208c7ce5b9b
Kuleb9lka <bogdan.lopatenko@gmail.com>
2024-02-03 11:59:34 +0300

    added constant

--- src/main/java/syberry/hackathon/BankSolutionApplication.java
+++ src/main/java/syberry/hackathon/BankSolutionApplication.java
@@ -1,4 +1,4 @@
-package syberry.hackathon.bank;
+package syberry.hackathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
